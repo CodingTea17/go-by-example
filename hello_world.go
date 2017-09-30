f46bc84d3451ee0d5d39785d48d97f5f674aaa8c
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-09-30 08:58:35 -0700

    kick off my intro to go with a hello world program

---
+++ hello_world.go
@@ -0,0 +1,20 @@
+package main
+
+/***************************************************
+** See me GO!
+**
+** Compile with `go build hello_world.go`
+** The above creates an executable `./hello_world`
+**
+** OR
+**
+** Just run `go run hello_world.go`
+**
+** OUTPUT: "hello world"
+****************************************************/
+
+import "fmt"
+
+func main() {
+  fmt.Println("hello world")
+}
